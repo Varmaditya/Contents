18ee3fa45fde044176db2c0ceac294735ef392fb
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2025-11-28 16:47:26 +0530

    Create Motivation.java

---
+++ Java/PracticePrograms/Motivation.java
@@ -0,0 +1,7 @@
+public class Motivation {
+    public static void main(String[] args) {
+        System.out.println("Here is a quote:");
+        System.out.println("\t\"The journey of a thousand miles begins with a single step.\"");
+        System.out.println("\nHappy Programming.");
+    }
+}
