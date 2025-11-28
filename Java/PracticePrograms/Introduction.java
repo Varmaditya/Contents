cfcacd2ff10c9ebe9f3bbec06e166898ad66f1d9
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2025-11-28 16:46:24 +0530

    Update Introduction.java

--- Java/PracticePrograms/Introduction.java
+++ Java/PracticePrograms/Introduction.java
@@ -1,7 +1,6 @@
 public class Introduction {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Programming!");
-        System.out.println("This is your second practice program.");
         System.out.println("Keep learning, keep growing!");
     }
 }
