c54c795dd2b72380081c788a38cd198dafaac60f
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2025-11-29 16:41:02 +0530

    Delete Python/PracticePrograms directory

--- Python/PracticePrograms/areaOfTriangle.py
+++
@@ -1,9 +0,0 @@
-# Program to calculate the area of a triangle
-
-baseLength = float(input("Enter the base of the triangle (in units): "))
-heightLength = float(input("Enter the height of the triangle (in units): "))
-
-# Area formula: 0.5 * base * height
-area = 0.5 * baseLength * heightLength
-
-print("The area of the triangle is:", area, "square units")
--- Python/PracticePrograms/perimeterOfSquare.py
+++
@@ -1,8 +0,0 @@
-# Program to calculate the perimeter of a square
-
-sideLength = float(input("Enter the side length of the square (in units): "))
-
-# Perimeter formula: 4 * side
-perimeter = 4 * sideLength
-
-print("The perimeter of the square is:", perimeter, "units")
