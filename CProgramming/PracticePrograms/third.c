fe54e3bb5d92e51c4dc7974c8e55900794f18299
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-08 17:00:42 +0530

    Create third.c

---
+++ CProgramming/PracticePrograms/third.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Line 1: Learning C syntax.\n");
+    printf("Line 2: Every statement ends with a semicolon.\n");
+    printf("Line 3: This is on a new line!");
+    
+    return 0;
+}
