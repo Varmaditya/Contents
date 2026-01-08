adf3b4174a5f21c89e7377b8990e2d53d06a1735
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-08 17:00:08 +0530

    Create second.c

---
+++ CProgramming/PracticePrograms/second.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // This is a comment - the compiler ignores this
+    printf("Welcome to C Programming Class!\n");
+    
+    return 0; // Signals that the program finished successfully
+}
