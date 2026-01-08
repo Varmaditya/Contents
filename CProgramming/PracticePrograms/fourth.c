b790281f64f6f027cfc9c728c9eddd7425bce53b
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-08 17:01:11 +0530

    Create fourth.c

---
+++ CProgramming/PracticePrograms/fourth.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Items List:\n");
+    printf("Item 1:\tApples\n");
+    printf("Item 2:\tOranges\n");
+    
+    printf("\nHe said, \"C programming is fun!\"");
+    
+    return 0;
+}
