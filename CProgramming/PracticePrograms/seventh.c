6b090ac51ec8f3ed7174a78566825d9246f5bc76
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-14 17:07:27 +0530

    Create seventh.c

---
+++ CProgramming/PracticePrograms/seventh.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Roll No\tName\tMarks\n");
+    printf("1\tAmit\t85\n");
+    printf("2\tRiya\t90\n");
+    printf("3\tKaran\t78\n");
+
+    return 0;
+}
