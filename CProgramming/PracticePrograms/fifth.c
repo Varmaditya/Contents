de37c13bc3f50a44a86f23fc9e5786dc382e8557
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-14 16:57:51 +0530

    Create fifth.c

---
+++ CProgramming/PracticePrograms/fifth.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("C Programming\n");
+    printf("Escape Sequences Demo\n\n");
+
+    printf("Name:\tJohn\n");
+    printf("Course:\tC Programming\n");
+
+    printf("Line1\vLine2\n");
+    printf("Page1\fPage2\n");
+
+    return 0;
+}
