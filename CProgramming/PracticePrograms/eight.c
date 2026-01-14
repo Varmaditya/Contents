1b8043f6ef7fe638b03b1a0eaf9d4e879a91496e
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-14 17:08:52 +0530

    Create eight.c

---
+++ CProgramming/PracticePrograms/eight.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Octal value \\101 gives: \101\n");
+    printf("Octal value \\102 gives: \102\n");
+
+    printf("Hex value \\x41 gives: \x41\n");
+    printf("Hex value \\x42 gives: \x42\n");
+
+    return 0;
+}
