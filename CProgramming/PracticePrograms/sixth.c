92cfd32db33f7119f9ad31b0cf59c2409b3c4c30
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-14 16:58:48 +0530

    Create sixth.c

---
+++ CProgramming/PracticePrograms/sixth.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("She said, \"C is easy.\"\n");
+    printf("It\'s fun to learn C.\n");
+    printf("File path: C:\\Programs\\C\\\n");
+    printf("What is C\\?\n");
+
+    return 0;
+}
