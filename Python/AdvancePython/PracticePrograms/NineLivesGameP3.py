f715fbe0c8ac31a8db7a0cebb1aede874291d536
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-26 18:20:42 +0530

    Add files via upload

---
+++ Python/AdvancePython/PracticePrograms/NineLivesGameP3.py
@@ -0,0 +1,7 @@
+import random
+
+lives = 9
+words = ['pizza', 'fairy', 'teeth', 'shirt', 'otter', 'plane']
+secretWord = random.choice(words)
+clue = list('?????')
+heartSymbol
---
+++ Python/AdvancePython/PracticePrograms/PasswordPickerP2.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'red',
+              'orange', 'yellow', 'green','blue', 'purple', 'fluffy',
+              'white', 'proud', 'brave']
+
+nouns = ['apple', 'dinosaur', 'ball', 'toaster', 'goat', 'dragon',
+         'hammer', 'duck', 'panda']
+
+print("Welcome to Password Picker!")
+
+while True:
+    adjective = random.choice(adjectives)
+    noun = random.choice(nouns)
+    number = random.randrange(0, 100)
+    specialChar = random.choice(string.punctuation)
+
+    password = adjective + noun + str(number) + specialChar
+    print("Your new password is: ", password)
+
+    response = input("Would you like another password? (y/n): ")
+    if response == "n":
+        break
