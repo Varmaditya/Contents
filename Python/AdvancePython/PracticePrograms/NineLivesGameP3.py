1e8c6fbb5c204bb2e70197788d9b92e57da2835c
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-01-29 16:54:05 +0530

    Update NineLivesGameP3.py

--- Python/AdvancePython/PracticePrograms/NineLivesGameP3.py
+++ Python/AdvancePython/PracticePrograms/NineLivesGameP3.py
@@ -4,4 +4,41 @@ lives = 9
 words = ['pizza', 'fairy', 'teeth', 'shirt', 'otter', 'plane']
 secretWord = random.choice(words)
 clue = list('?????')
-heartSymbol
+heartSymbol = u'\u2764'
+guessedWordCorrectly = False
+unknownLetters = len(secretWord)
+
+def update_clue(guessedLetter, secretWord, clue, unknownLetters):
+    index = 0
+
+    while index < len(secretWord):
+        if guessedLetter == secretWord[index]:
+            clue[index] = guessedLetter
+            unknownLetters = unknownLetters - 1
+        index = index + 1
+
+    return unknownLetters
+
+while lives > 0:
+    print(clue)
+    print("Lives left: " + heartSymbol * lives)
+    guess = input("Guess a letter or the whole word: ")
+
+    if guess == secretWord:
+        guessedWordCorrectly = True
+        break
+
+    if guess in secretWord:
+        unknownLetters = update_clue(guess, secretWord, clue, unknownLetters)
+    else:
+        print("Incorrect. You lose a life!")
+        lives = lives - 1
+
+    if unknownLetters == 0:
+        guessedWordCorrectly = True
+        break
+
+if guessedWordCorrectly:
+    print("You won! The secret word was: " + secretWord)
+else:
+    print("You lost! The secret word was: " + secretWord)
