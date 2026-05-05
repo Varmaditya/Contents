0de4c918786d78c4a1c6f83068fa97f4629b359d
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-05-05 19:31:58 +0530

    Update punP1.cpp

--- C++/PracticePrograms/punP1.cpp
+++ C++/PracticePrograms/punP1.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "\nHello " << name << "!\n";
+    cout << "To C++, or not to C++: that is the question.\n";
+
+    return 0;
+}
