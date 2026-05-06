7a890f6f69570e850d2abc335bcb0bd1cb527cb9
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-05-06 19:09:59 +0530

    Update digitSplitterP16.cpp

--- C++/PracticePrograms/digitSplitterP16.cpp
+++ C++/PracticePrograms/digitSplitterP16.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int number;
+
+    int hundredsDigit;
+    int tensDigit;
+    int unitsDigit;
+
+    cout << "Enter a 3-digit number: ";
+    cin >> number;
+
+    hundredsDigit = number / 100;
+    tensDigit = (number / 10) % 10;
+    unitsDigit = number % 10;
+
+    cout << "\nHundreds = " << hundredsDigit << endl;
+    cout << "Tens = " << tensDigit << endl;
+    cout << "Units = " << unitsDigit << endl;
+
+    return 0;
+}
