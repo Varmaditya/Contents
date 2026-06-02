0800bc3f600f26e5a00813e3e7a4b2345a16b537
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-02 19:11:54 +0530

    Create Arrays3.cpp

---
+++ C++/LessonPrograms/Arrays&Strings/Arrays3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numbers[5];
+
+    // Taking input into array
+    cout << "Enter 5 numbers:\n";
+
+    for (int i = 0; i < 5; i++) {
+        cin >> numbers[i];
+    }
+
+    // Displaying array elements
+    cout << "\nElements in the array are:\n";
+
+    for (int i = 0; i < 5; i++) {
+        cout << "numbers[" << i << "] = "
+             << numbers[i] << endl;
+    }
+
+    return 0;
+}
