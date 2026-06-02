79acc230a682cfe7bb610054b46b5f29d1a6f03c
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-02 19:10:45 +0530

    Update Arrays2.cpp

--- C++/LessonPrograms/Arrays&Strings/Arrays2.cpp
+++ C++/LessonPrograms/Arrays&Strings/Arrays2.cpp
@@ -22,45 +22,35 @@ int main() {
     cout << "Integer Array:\n";
 
     for (int i = 0; i < 3; i++) {
-
         cout << numbers[i] << endl;
-
     }
 
     // Displaying float array
     cout << "\nFloat Array:\n";
 
     for (int i = 0; i < 3; i++) {
-
         cout << prices[i] << endl;
-
     }
 
     // Displaying character array
     cout << "\nCharacter Array:\n";
 
     for (int i = 0; i < 3; i++) {
-
         cout << grades[i] << endl;
-
     }
 
     // Displaying boolean array
     cout << "\nBoolean Array:\n";
 
     for (int i = 0; i < 3; i++) {
-
         cout << status[i] << endl;
-
     }
 
     // Displaying string array
     cout << "\nString Array:\n";
 
     for (int i = 0; i < 3; i++) {
-
         cout << names[i] << endl;
-
     }
 
     return 0;
