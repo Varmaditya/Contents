fef8ced00833c9a6bf7a79c6194f41d72074c23d
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-02 19:09:08 +0530

    Create Arrays&Strings/Arrays1.cpp

---
+++ C++/LessonPrograms/Arrays&Strings/Arrays1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Declaring an array
+    int marks[5];
+
+    // Initializing array elements
+    marks[0] = 90;
+    marks[1] = 85;
+    marks[2] = 78;
+    marks[3] = 92;
+    marks[4] = 88;
+
+    // Displaying array elements
+    cout << "Marks stored in array:\n";
+
+    for (int i = 0; i < 5; i++) {
+
+        cout << "marks[" << i << "] = "
+             << marks[i] << endl;
+
+    }
+
+    return 0;
+}
