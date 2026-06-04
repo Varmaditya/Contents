e86ba14040a67ea3ef143af0be4190300a067c75
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-04 19:12:33 +0530

    Create Strings1.cpp

---
+++ C++/LessonPrograms/Arrays&Strings/Strings1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // Declaring strings
+    string firstName;
+    string lastName;
+
+    // Initializing strings
+    firstName = "John";
+    lastName = "Doe";
+
+    // Displaying strings
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+
+    // Direct initialization
+    string course = "C++ Programming";
+
+    cout << "Course: " << course << endl;
+
+    return 0;
+}
