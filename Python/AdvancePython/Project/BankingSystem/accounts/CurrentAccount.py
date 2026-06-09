c58ce8891fb7c705bddeffe7c9e408f7dd35daff
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-09 14:41:46 +0530

    Create CurrentAccount.py

---
+++ Python/AdvancePython/Project/BankingSystem/accounts/CurrentAccount.py
@@ -0,0 +1,22 @@
+# accounts/CurrentAccount.py
+
+from accounts.Account import Account
+
+
+class CurrentAccount(Account):
+
+    def __init__(self, account_no, customer_id, balance=0):
+
+        super().__init__(account_no, customer_id, balance)
+
+        self.account_type = "CURRENT"
+
+    # CONVERT TO DICTIONARY
+    def to_dict(self):
+
+        return {
+            "account_no": self.account_no,
+            "customer_id": self.customer_id,
+            "account_type": self.account_type,
+            "balance": self.balance
+        }
