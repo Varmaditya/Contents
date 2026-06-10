1908447055f784b1d98a917337966ae2590e2aee
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-10 17:54:18 +0530

    Add files via upload

--- CProgramming/LessonPrograms/Basics/FormatSpecifiers.c
+++ CProgramming/LessonPrograms/Basics/FormatSpecifiers.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <stddef.h>
 
 int main(void) {
 
     int n = 123;
-    unsigned int u = 40000u;
     long int L = 2000000000L;
-    long long int LL = 900000000000LL;
     float f = 3.14159f;
     double d = 2.718281828;
     char ch = 'C';
@@ -14,9 +11,7 @@ int main(void) {
 
     // Printing different data types
     printf("int: %d\n", n);
-    printf("unsigned: %u\n", u);
     printf("long: %ld\n", L);
-    printf("long long: %lld\n", LL);
     printf("float: %f\n", f);
     printf("double: %lf\n", d);
     printf("char: %c\n", ch);
@@ -30,10 +25,6 @@ int main(void) {
     // Printing expressions
     printf("2 + 3 = %d\n", 2 + 3);
     printf("Area 7 x 5 = %d\n", 7 * 5);
-
-    // Sizes and addresses
-    printf("sizeof(int) = %zu bytes\n", sizeof(int));
-    printf("address of n = %p\n", (void*)&n);
 
     return 0;
 }
