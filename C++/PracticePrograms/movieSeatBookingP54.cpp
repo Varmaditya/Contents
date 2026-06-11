d401c0e5c6c7db57d3eed05e4f59102f47eef266
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-11 17:41:35 +0530

    Create movieSeatBookingP54.cpp

---
+++ C++/PracticePrograms/movieSeatBookingP54.cpp
@@ -0,0 +1,53 @@
+/*
+Program: Movie Theater Seat Booking System
+Description: Allows users to book seats and view seat availability.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int seats[10] = {0};
+
+    int seatNumber;
+
+    cout << "Available Seats:\n";
+
+    for(int index = 0; index < 10; index++) {
+
+        cout << "Seat "
+             << index + 1
+             << " : ";
+
+        if(seats[index] == 0)
+            cout << "Available";
+        else
+            cout << "Booked";
+
+        cout << endl;
+    }
+
+    cout << "\nEnter Seat Number to Book: ";
+    cin >> seatNumber;
+
+    seats[seatNumber - 1] = 1;
+
+    cout << "\nUpdated Seat Status:\n";
+
+    for(int index = 0; index < 10; index++) {
+
+        cout << "Seat "
+             << index + 1
+             << " : ";
+
+        if(seats[index] == 0)
+            cout << "Available";
+        else
+            cout << "Booked";
+
+        cout << endl;
+    }
+
+    return 0;
+}
