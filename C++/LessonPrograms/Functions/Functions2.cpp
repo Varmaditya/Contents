c8940d8e31effd8c3aeeee9254fac368c3cad27f
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-17 19:09:06 +0530

    Create Functions2.cpp

---
+++ C++/LessonPrograms/Functions/Functions2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Function Definition
+void displayStudent(string name, int age) {
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+
+}
+
+int main() {
+
+    // Passing Arguments
+    displayStudent("Aditya", 23);
+
+    return 0;
+}
