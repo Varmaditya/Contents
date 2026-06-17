cf639843ba0394fd35ef59a90ab813388fbbfa9c
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-17 19:15:22 +0530

    Create Functions10.cpp

---
+++ C++/LessonPrograms/Functions/Functions10.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Recursive Function
+int sum(int n) {
+
+    // Base Case
+    if (n == 1) {
+
+        return 1;
+
+    }
+
+    // Recursive Case
+    return n + sum(n - 1);
+
+}
+
+int main() {
+
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Sum = "
+         << sum(num);
+
+    return 0;
+}
