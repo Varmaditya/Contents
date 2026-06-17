6ae2b06c589746434f29f4d9de8d7d3d59748cbf
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-17 19:10:31 +0530

    Create Functions4.cpp

---
+++ C++/LessonPrograms/Functions/Functions4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Function that returns nothing
+void showMessage() {
+
+    cout << "Learning Functions in C++" << endl;
+
+}
+
+int main() {
+
+    showMessage();
+
+    return 0;
+}
