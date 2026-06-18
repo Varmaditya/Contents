17eb8dcd726e70ac69d05b80a171fb80ab886bf9
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-18 15:31:32 +0530

    Add Pointers1.cpp to demonstrate pointers usage

---
+++ C++/LessonPrograms/Pointers/Pointers1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+    int num = 50;
+
+    cout << "Value of num: " << num << endl;
+    cout << "Address of num: " << &num << endl;
+
+    return 0;
+}
