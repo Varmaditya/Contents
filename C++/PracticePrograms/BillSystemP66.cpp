5f36b3857652c09ba7b869f5042676a2ba5dd4b9
Aditya Vijaybhan Varma <74001713+Varmaditya@users.noreply.github.com>
2026-06-26 18:14:39 +0530

    Update BillSystemP66.cpp

--- C++/PracticePrograms/BillSystemP66.cpp
+++ C++/PracticePrograms/BillSystemP66.cpp
@@ -9,53 +9,40 @@ using namespace std;
 
 // Display menu
 void displayMenu() {
-
     cout << "=========== MENU ===========" << endl;
     cout << "1. Burger    ₹120" << endl;
     cout << "2. Pizza     ₹250" << endl;
     cout << "3. Pasta     ₹180" << endl;
     cout << "4. Coffee    ₹90" << endl;
     cout << "============================" << endl;
-
+l
 }
 
 // Calculate bill
 int calculateBill(int price, int quantity) {
-
     return price * quantity;
-
 }
 
 // Apply discount
 float applyDiscount(float totalBill) {
-
     if(totalBill >= 500)
         return totalBill * 0.10;
 
     return 0;
-
 }
 
 // Print receipt
 void printReceipt(float totalBill,
                   float discount,
                   float finalBill) {
-
     cout << "\n========= RECEIPT =========\n";
 
-    cout << "Total Bill : ₹"
-         << totalBill << endl;
-
-    cout << "Discount : ₹"
-         << discount << endl;
-
-    cout << "Final Bill : ₹"
-         << finalBill << endl;
-
+    cout << "Total Bill : ₹"  << totalBill << endl;
+    cout << "Discount : ₹" << discount << endl;
+    cout << "Final Bill : ₹" << finalBill << endl;
 }
 
 int main() {
-
     int prices[4]={120,250,180,90};
 
     int foodChoice;
@@ -75,8 +62,5 @@ int main() {
     float discount =
     applyDiscount(totalBill);
 
-    printReceipt(totalBill,
-                 discount,
-                 totalBill-discount);
-
+    printReceipt(totalBill, discount, totalBill-discount);
 }
